1e8964bd59a7b390e7cc41c456b9956bc9ddab1b
Hyup <kkddong98@gmail.com>
2021-05-13 12:32:13 +0900

    8th

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -5,80 +5,73 @@
 
 namespace lab2
 {
-    void PrintIntegers(std::istream& in, std::ostream& out)
-    {
-        int tem;
-        std::string trash;
-        out << std::setfill(' ') << std::setw(12) << std::right << "oct" << ' '
-            << std::setw(10) << std::right << "dec" << ' '
-            << std::setw(8) << std::right << "hex"  << std::endl;
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		int tem;
+		std::string trash;
 
-        out << std::setfill('-') << std::setw(13) << ' '
-            << std::setw(11) << ' '
-            << std::setw(8) << "" << std::endl;
+		out << std::setfill(' ') << std::setw(12) << std::right << "oct" << ' '	<< std::setw(10) << std::right << "dec" << ' ' << std::setw(8) << std::right << "hex" << std::endl;
 
-        while (!in.eof())
-        {
-            in >> tem;
+		out << std::setfill('-') << std::setw(13) << ' ' << std::setw(11) << ' ' << std::setw(8) << "" << std::endl;
 
+		while (!in.eof())
+		{
+			in >> tem;
 
-            if (in.fail())
-            {
-                in.clear();
-                std::getline(in, trash, ' ');//수정 필요 할 수도
-            }
-            else
-            {
-                
-                out << std::uppercase << std::setfill(' ') << std::setw(12) << std::right << std::oct << tem << ' '
-                    << std::setfill(' ') << std::setw(10) << std::right << std::dec << tem << ' '
-                    << std::setfill(' ') << std::setw(8) << std::right <<std::hex << tem  << std::endl;
-            }
-        }
-    }
+			if (in.fail())
+			{
+				in.clear();
+				std::getline(in, trash, ' ');
+			}
 
-    void PrintMaxFloat(std::istream& in, std::ostream& out)
-    {
-        float max = 0;
-        float tem;
-        std::string trash;
-        int flag = 0;
+			else
+			{
 
-        while (!in.eof())
-        {
-            in >> tem;
+				out << std::uppercase << std::setfill(' ') << std::setw(12) << std::right << std::oct << tem << ' '	<< std::setfill(' ') << std::setw(10) << std::right << std::dec << tem << ' ' << std::setfill(' ') << std::setw(8) << std::right << std::hex << tem << std::endl;
+			}
+		}
+	}
 
-            if (in.fail())
-            {
-                in.clear();
-                std::getline(in, trash, ' ');//수정 필요 할 수도
-            }
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		float max = 0;
+		float tem;
+		std::string trash;
+		int flag = 0;
 
-            else
-            {
+		while (!in.eof())
+		{
+			in >> tem;
 
-                if (flag == 0)
-                {
-                    max = tem;
-                    flag++;
-                }
+			if (in.fail())
+			{
+				in.clear();
+				std::getline(in, trash, ' ');//수정 필요 할 수도
+			}
 
-                else if (flag != 0)
-                {
-                    if (max <= tem)
-                    {
-                        max = tem;
-                    }
-                }
+			else
+			{
 
-                out << std::showpoint << std::fixed << std::setprecision(3) << std::showpos << std::setfill(' ') << std::setw(5) << std::left << ""
-                    << std::setfill(' ') << std::setw(15) << std::internal << tem << "\n";
-            }
-        }
-       
-        out << std::showpoint << std::fixed << std::setprecision(3) << std::showpos << std::setfill(' ') << std::setw(5) << std::left << "max:"
-            << std::setfill(' ') << std::setw(15) << std::internal << max << "\n";
+				if (flag == 0)
+				{
+					max = tem;
+					flag++;
+				}
 
-    }
+				else if (flag != 0)
+				{
+					if (max <= tem)
+					{
+						max = tem;
+					}
+				}
+
+				out << std::showpoint << std::fixed << std::setprecision(3) << std::showpos << std::setfill(' ') << std::setw(5) << std::left << "" << std::setfill(' ') << std::setw(15) << std::internal << tem << "\n";
+			}
+		}
+
+		out << std::showpoint << std::fixed << std::setprecision(3) << std::showpos << std::setfill(' ') << std::setw(5) << std::left << "max:" << std::setfill(' ') << std::setw(15) << std::internal << max << "\n";
+
+	}
 
 }
