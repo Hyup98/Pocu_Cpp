ea17f3548f6a0bf1ae4837850a53cb9b762582af
Hyup <kkddong98@gmail.com>
2021-07-27 13:48:12 +0900

    no message

---
+++ Lab11/Storage.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <memory>
+
+namespace lab11
+{
+	template<typename T>
+	class Storage
+	{
+	public:
+		Storage(unsigned int length);
+		Storage(unsigned int length, const T& initialValue);
+		Storage(Storage&& other);
+
+		void operator=(Storage&& other);
+		bool Update(unsigned int index, const T& data);
+		const std::unique_ptr<T[]>& GetData() const;
+		unsigned int GetSize() const;
+		T* mArray;
+		unsigned int mArraySize;
+	};
+
+	template<typename T>
+	Storage<T>::Storage(unsigned int length)
+		: mArraySize(length)
+	{
+		mArray = new T[length];
+		memset(mArray, 0, length * sizeof(T));
+	}
+
+	template<typename T>
+	Storage<T>::Storage(unsigned int length, const T& initialValue)
+		: mArraySize(0)
+	{
+		mArray = new T[length];
+		memset(mArray, initialValue, length * sizeof(T));
+	}
+	
+	template<typename T>
+	Storage<T>::Storage(Storage&& other)
+		: mArray(other.mArray)
+		, mArraySize(other.mArraySize)
+	{
+		other.mArray = nullptr;
+		other.mArraySize = 0;
+	}
+
+	template<typename T>
+	void Storage<T>::operator=(Storage&& other)
+	{
+		mArraySize = other.GetSize;
+		delete mArray;
+		mArray = other.mArray;
+		other.mArray = nullptr;
+		other.mArraySize = 0;
+	}
+
+	template<typename T>
+	bool Storage<T>::Update(unsigned int index, const T& data)
+	{
+		if (index < mArraySize)
+		{
+			mArray[index] = data;
+			return true;
+		}
+		return false;
+	}
+
+	template<typename T>
+	const std::unique_ptr<T[]>& Storage<T>::GetData() const
+	{
+		std::unique_ptr<T[]> temp = std::make_unique<T[]>(mArraySize);
+		for (int i = 0; i < mArraySize; i++)
+		{
+			temp[i] = mArray[i];
+		}
+		
+		return std::move(temp);
+	}
+
+	template<typename T>
+	unsigned int Storage<T>::GetSize() const
+	{
+		return mArraySize;
+	}
+}
--- Lab11/main.cpp
+++ Lab11/main.cpp
@@ -1,4 +1,88 @@
+#include <cassert>
+
+#include "Storage.h"
+
+
+using namespace lab11;
+
 int main()
 {
+	const unsigned int SIZE = 3;
 
+	Storage<int> storage1(SIZE);
+	const std::unique_ptr<int[]>& data1 = storage1.GetData();
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		assert(storage1.mArray[i] == 0);
+		assert(data1[i] == 0);	
+	}
+
+
+	Storage<int> storage2(SIZE, 100);
+	const std::unique_ptr<int[]>& data2 = storage2.GetData();
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		assert(data2[i] == 100);
+	}
+
+	// using move constructor
+	Storage<int> storage3(SIZE, 50);
+	const std::unique_ptr<int[]>& data3 = storage3.GetData();
+
+	assert(storage3.GetSize() == SIZE);
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		assert(data3[i] == 50);
+	}
+
+	Storage<int> storage3Copy(std::move(storage3));
+	const std::unique_ptr<int[]>& data3Copy = storage3Copy.GetData();
+
+	assert(storage3Copy.GetSize() == SIZE);
+	for (int i = 0; i < SIZE; ++i)
+	{
+		assert(data3Copy[i] == 50);
+	}
+
+	assert(storage3.GetData() == nullptr);
+	assert(storage3.GetSize() == 0);
+
+	Storage<char> storage4(SIZE, 'f');
+	const std::unique_ptr<char[]>& data4 = storage4.GetData();
+
+	assert(storage4.GetSize() == SIZE);
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		assert(data4[i] == 'f');
+	}
+
+	Storage<char> storage4Copy = std::move(storage4);
+	const std::unique_ptr<char[]>& data4Copy = storage4Copy.GetData();
+
+	assert(storage4Copy.GetSize() == SIZE);
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		assert(data4Copy[i] == 'f');
+	}
+
+	assert(storage4.GetData() == nullptr);
+	assert(storage4.GetSize() == 0);
+
+	const unsigned int SIZE2 = 5000;
+
+	Storage<int> storage5(SIZE2);
+	const std::unique_ptr<int[]>& data5 = storage5.GetData();
+
+	for (int i = 0; i < SIZE2; ++i)
+	{
+		storage5.Update(i, SIZE2 - i - 1);
+		assert(data5[i] == SIZE2 - i - 1);
+	}
+
+	return 0;
 }
