88cd8781f17e088435bfa388c2a9b0e7588cae5d
Hyup <kkddong98@gmail.com>
2021-08-07 19:08:55 +0900

    no message

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -1,26 +1,50 @@
-#include<iostream>
 #include <cassert>
-#include "QueueStack.h"
+#include <cmath>
+
 #include "SmartStack.h"
-#include"SmartQueue.h"
 
-
-using namespace std;
 using namespace assignment3;
 
 int main()
 {
-    QueueStack<int> qs(3);
-    qs.Enqueue(1); // [ [ 1 ] ]
-    qs.Enqueue(2); // [ [ 1, 2 ] ]
-    qs.Enqueue(3); // [ [ 1, 2, 3 ] ]
-    qs.Enqueue(4); // [ [ 1, 2, 3 ], [ 4 ] ]
-    qs.Enqueue(5); // [ [ 1, 2, 3 ], [ 4, 5 ] ]
+	const double EPSILON = 0.0009765625;
 
-    int dequeued = qs.Dequeue(); // dequeued의 값은 3. qs의 요소: [ [ 1, 2 ], [ 4, 5 ] ]
-    
-    cout<< qs.GetAverage(); 
-    int a;
-    cin >> a;
-    return 0;
+	SmartStack<float> ss;
+
+	ss.Push(3.4999f);
+	ss.Push(1.2f);
+	ss.Push(4.6555f);
+	ss.Push(3.3299f);
+	ss.Push(10.2f);
+	ss.Push(1.1f);
+	ss.Push(-5.9f);
+	ss.Push(1.1f);
+	ss.Push(-12.4f);
+	ss.Push(9.2f);
+
+	assert(ss.GetCount() == 10U);
+	assert(ss.Peek() == 9.2f);
+	assert(ss.GetMax() == 10.2f);
+	assert(ss.GetMin() == -12.4f);
+	assert(std::abs(ss.GetSum() - 15.985301f) <= EPSILON);
+	assert(std::abs(ss.GetAverage() - 1.599) <= EPSILON);
+	assert(std::abs(ss.GetVariance() - 40.057) <= EPSILON);
+	assert(std::abs(ss.GetStandardDeviation() - 6.329) <= EPSILON);
+	assert(ss.Peek() == 9.2f);
+
+	float popped1 = ss.Pop();
+	float popped2 = ss.Pop();
+
+	assert(popped1 == 9.2f);
+	assert(popped2 == -12.4f);
+	assert(ss.GetCount() == 8U);
+	assert(ss.Peek() == 1.1f);
+	assert(ss.GetMax() == 10.2f);
+	assert(ss.GetMin() == -5.9f);
+	assert(std::abs(ss.GetSum() - 19.1853008f) <= EPSILON);
+	assert(std::abs(ss.GetAverage() - 2.398) <= EPSILON);
+	assert(std::abs(ss.GetVariance() - 17.714) <= EPSILON);
+	assert(std::abs(ss.GetStandardDeviation() - 4.209) <= EPSILON);
+
+	return 0;
 }
